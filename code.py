515336ff97115a9fd2a465787ba06e4622980815
nikita <nup@somaiya.edu>
2022-06-28 14:28:24 +0530

    added email field

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-from tkinter import PhotoImage, Tk, Label
+from tkinter import Entry, PhotoImage, StringVar, Tk, Label
 
 def signup():
     window = Tk()
@@ -7,6 +7,12 @@ def signup():
     window.resizable(False, False)
     
     Label(window, text="Welcome to password manager", font="12").pack()
+    
+    Label(window,text="Email").place(x=100,y=50)
+    email = StringVar()
+    inputtxt = Entry(window, width = 20, textvariable = email)
+    inputtxt.place(x=160,y=50)
+
 
     window.mainloop()
 
