a7f94d5c0c37a10a4b770454544fdb4956416498
aryan <aaryan.p114@gmail.com>
2022-06-28 21:45:05 +0530

    signup functionality added

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
+import json
 from tkinter import Button, Entry, PhotoImage, StringVar, Tk, Label
+
+def addUser(password, email, window):
+    data["email"] = email
+    data["password"] = password
+    with open("code.json", "w") as f:
+        json.dump(data, f)
+    window.destroy()
 
 def signup():
     window = Tk()
     window.title("Signup - password manager")
     window.geometry("400x150")
     window.resizable(False, False)
+    photo = PhotoImage(file=".//assets//icons1.png")
+    window.iconphoto(False, photo)
     
     Label(window, text="Welcome to password manager", font="12").pack()
     
@@ -18,10 +28,15 @@ def signup():
     inputp = Entry(window, width = 20, show="*", textvariable=password)
     inputp.place(x=160,y=90)
 
-    Button(window,text="Signup").place(x=170,y=115)
-
-
-
+    Button(window, text="Signup", command=lambda: addUser(
+        password.get(), email.get(), window)).place(x=170, y=115)
     window.mainloop()
-
-signup()
+try:
+    with open("code.json", "r") as f:
+        data = json.load(f)
+except:
+    data = {}
+    with open("code.json", "w") as f:
+        json.dump(data, f)
+if len(data) == 0:
+    signup()
